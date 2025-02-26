80a3f023b31cdb977a4b6d8aca895267b03a71e5
Uklonsd <khubashvili12@gmail.com>
2025-02-26 10:35:57 +0200

    Done!

---
+++ INSTRUCTION.md
@@ -0,0 +1,8 @@
+```bash
+ chmod +x bootstrap.sh
+
+./bootstrap.sh
+
+kubectl get statefulsets -n mysql
+
+kubectl logs -n mysql mysql-0
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+kind create cluster --config cluster.yml
+
+kubectl create ns mysql
+kubectl apply -f .infrastrucuture/namespace.yml
+
+kubectl apply -f st-config.yml
+kubectl apply -f st-secret.yml
+kubectl apply -f st-service.yml
+kubectl apply -f stateful.yml
+
+kubectl apply -f configMap.yml
+kubectl apply -f secret.yml
+kubectl apply -f pv.yml
+kubectl apply -f pvc.yml
+kubectl apply -f nodeport.yml
+kubectl apply -f clusterIp.yml
+kubectl apply -f hpa.yml
+kubectl apply -f deployment.yml
